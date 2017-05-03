778b67f51eb102ae8aac0c0cbf09c93137add178
Veeteq <witold.wojnarowicz@gmail.com>
2017-05-03 19:09:00 +0200

    Initial commit

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@SpringBootApplication
+public class Main {
+
+	@Value("${spring.datasource.url}")
+	private String dbUrl;
+	
+	@RequestMapping("/")
+	  String index() {
+	    return "index";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+}
